34c9cac32cfee618151df75852b2e287f8031bd8
Gunaj1 <gunaj7e@gmail.com>
2025-08-04 01:51:00 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ComparisonProvider } from "./contexts/ComparisonContext";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 
+import { ComparisonProvider } from "./contexts/ComparisonContext";
+
+import Index from "./pages/Index";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 import PrivateRoute from "./components/PrivateRoute";
 
 const queryClient = new QueryClient();
@@ -20,8 +21,20 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
+            {/* Public Route */}
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={<PrivateRoute><Index /></PrivateRoute>} />
+
+            {/* Protected Route */}
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Index />
+                </PrivateRoute>
+              }
+            />
+
+            {/* Catch-all */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
