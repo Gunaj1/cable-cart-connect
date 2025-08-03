82c6ed95597c94c2c5b1916d36f55b5ee337524c
Gunaj1 <gunaj7e@gmail.com>
2025-08-04 00:52:40 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import Login from "./pages/Login";
+// src/App.tsx
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,6 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ComparisonProvider } from "./contexts/ComparisonContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login"; // ✅ Add this import
 
 const queryClient = new QueryClient();
 
@@ -19,8 +21,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login />} /> {/* ✅ Add this route */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
