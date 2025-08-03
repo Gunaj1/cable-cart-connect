6d7c33c6736a9269ee5ab8a8c93e43a466e6a3ae
Gunaj1 <gunaj7e@gmail.com>
2025-08-04 01:54:38 +0530

    PrivateRoute.tsx

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -2,7 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-// Replace with your actual keys
 const supabase = createClient(
   "https://hddvrvhuemdqahfhgsnf.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkZHZydmh1ZW1kcWFoZmhnc25mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNDQ0MDIsImV4cCI6MjA2OTgyMDQwMn0.c8-0Aik8Si2tcP2jbs7NWs9ruMaNwWjGJI8MpswCDwA
